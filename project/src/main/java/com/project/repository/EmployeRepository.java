ead27dcad1579eb7ef32f557bf085938ce33d31e
Uday Shastrakar <uday.shastrakar@gmail.com>
2023-03-12 22:22:25 +0530

    deelet3

--- project/src/main/java/com/project/repository/EmployeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.model.Employe;
-
-public interface EmployeRepository extends JpaRepository<Employe,Long> {
-
-}
